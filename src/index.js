0029c66e61137b162c25f4a6fabc6d192bf666d5
Valerie325 <morevimgh14@gmail.com>
2024-11-28 11:18:56 +0000

    added js

---
+++ src/index.js
@@ -0,0 +1,27 @@
+let name =("Valerie-Anne");
+console.log(name);
+let age =(18 + "years old" );
+console.log(age);
+let x=9
+let y =5
+let results =(x+y);
+console.log(results);
+let num1 =(10);
+let num2 =(5);
+let total =(num1-num2);
+console.log(total);
+let a =40;
+a /= 10;
+console.log(a);
+let l =(7);
+let j =(8);
+let ans =(l*j);
+console.log(ans);
+let f = 15%4;
+console.log(f)
+let game =true; //Boolean
+console.log(typeof{game});
+var number = prompt("How many shoes will you buy");
+console.log(number + "shoes" );
+let color = prompt("what is your favourite color?");
+console.log(color);
